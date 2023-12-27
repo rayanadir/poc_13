6a53014f10d4d655f41a9abee09ee2ada2c5c623
rayanadir <rayan.dahmena@gmail.com>
2023-12-27 20:15:34 +0100

    add cors configuration

---
+++ server/src/main/java/com/rayan/server/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.rayan.server.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        http
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+                    configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+                    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type","Content-Type"));
+                    return configuration;
+                }))
+                .csrf(c -> c.disable())
+                .authorizeRequests()
+                .requestMatchers("/**").permitAll();
+        return http.build();
+    }
+}

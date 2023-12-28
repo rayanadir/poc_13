424e02f5209b5383aa79804e782a773f5cef2d60
rayanadir <rayan.dahmena@gmail.com>
2023-12-29 00:01:02 +0100

    update chat repository

--- server/src/main/java/com/rayan/server/repositories/ChatRepository.java
+++ server/src/main/java/com/rayan/server/repositories/ChatRepository.java
@@ -3,7 +3,6 @@ package com.rayan.server.repositories;
 import com.rayan.server.models.Chat;
 import com.rayan.server.models.Conversation;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -13,10 +12,4 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
      * Find all chat messages from a conversation
      */
     List<Chat> findByConversation(Conversation conversation);
-
-    /**
-     * Get last chat conversation message sent
-     */
-    //@Query(nativeQuery = true, value = "SELECT * from chat where conversationid=")
-    //Chat findByConversationAndOrderByCreatedatDesc(Conversation conversation);
 }

2ac21ab033d7b0b69961db22489d53c5c2c9efe9
rayanadir <rayan.dahmena@gmail.com>
2023-12-28 01:33:45 +0100

    send message

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,7 +5,8 @@ import { UserResponse } from './interfaces/UserResponse';
 import { UserObject } from './interfaces/UserObject';
 import { Conversation } from './interfaces/Conversation';
 import { Chat } from './interfaces/Chat';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ChatRequest } from './interfaces/ChatRequest';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,8 @@ export class AppComponent implements OnDestroy {
     api_url:string ="http://localhost:8080/api";
 
     users!:User[];
-    user: UserObject = {firstname:"", lastname:"",id:undefined,type:"",customerid:undefined, customerserviceid:undefined};
+    currentUser: UserObject | undefined //= {firstname:undefined, lastname:undefined,id:undefined,type:undefined,customerid:undefined, customerserviceid:undefined};
+    userObject: User | undefined = {id: 0,firstname: "",lastname: "",email: "",password: "",birthdate: undefined,type: ""}
     displayLoggedUser:string="";
     conversations: Conversation[] | undefined = [];
     currentConversation!: Conversation;
@@ -25,13 +27,8 @@ export class AppComponent implements OnDestroy {
     chat: Chat[] | undefined = [];
     chatMessage: Chat | undefined;
     selectedChat:boolean=false;
+    chatInput:string="";
 
-    form = this.fb.group({
-      chatInput: [
-        '',
-        [Validators.required]
-      ]
-    })
 
     constructor(private http: HttpClient, public fb: FormBuilder){
       // get all users
@@ -55,7 +52,8 @@ export class AppComponent implements OnDestroy {
           const {id,firstname,lastname,email,password,birthdate,type,createdat,updatedat} = userResponse; 
           const {address,customerid} = customer;
           //this.userObject = new Customer(id,firstname,lastname,email,password,birthdate,type,customerid,user.id,address);
-          this.user={id,firstname,lastname,type,customerid}
+          this.currentUser={id,firstname,lastname,type,customerid};
+          this.userObject={id,birthdate,email,firstname,lastname,password,type}
           this.displayLoggedUser=`${firstname} ${lastname}, id: ${id}, type:${type}, customer_id:${customerid}`;
           this.findUserConversations(id);
         }else if(userType==="customer_service"){
@@ -63,7 +61,8 @@ export class AppComponent implements OnDestroy {
           const customerService = response.customerServiceModel
           const {id,firstname,lastname,email,password,birthdate,type,createdat,updatedat} = userResponse;
           const {customerserviceid,agency,userid} = customerService;
-          this.user={id,firstname,lastname,type,customerserviceid}
+          this.currentUser={id,firstname,lastname,type,customerserviceid};
+          this.userObject={id,birthdate,email,firstname,lastname,password,type}
           this.displayLoggedUser=`${firstname} ${lastname}, id: ${id}, type:${type}, customer_service_id:${customerserviceid}`;
           //this.userObject = new CustomerService(id,firstname,lastname,email,password,birthdate,type,customerserviceid,userid,agency);
           this.findUserConversations(id)
@@ -73,14 +72,14 @@ export class AppComponent implements OnDestroy {
       
     }
 
-    findUserConversations(id:number):void{
-      this.http.get<Conversation[]>(`${this.api_url}/conversations/${this.user.type}/${id}`).subscribe((conversations: Conversation[]) => {
+    findUserConversations(id:number | undefined):void{
+      this.http.get<Conversation[]>(`${this.api_url}/conversations/${this.currentUser?.type}/${id}`).subscribe((conversations: Conversation[]) => {
         //console.log(conversations[0]);
         this.conversations=conversations.map((conversation) => {
           // get interlocutor name
-          this.user.type == "customer" ?
+          this.currentUser?.type == "customer" ?
             this.interlocutor= `${conversation.customerServiceModel.customerservice?.firstname} ${conversation.customerServiceModel.customerservice?.lastname}`
-          : this.user.type == "customer_service" ? this.interlocutor= `${conversation.customer.customer?.firstname} ${conversation.customer.customer?.lastname}` : undefined;
+          : this.currentUser?.type == "customer_service" ? this.interlocutor= `${conversation.customer.customer?.firstname} ${conversation.customer.customer?.lastname}` : undefined;
           // get last chat message sent
           return conversation;
         })
@@ -97,17 +96,22 @@ export class AppComponent implements OnDestroy {
         this.selectedChat=true
       })
     }
-
-    getUser(id:number){
-
+  
+    change(event:any){
+      this.chatInput=event.target.value
     }
 
-    sendChatMessage(/*id:Number, messageRequest:Chat*/):void{
-      console.log(this.currentConversation);
+    sendChatMessage():void{
+      let request: ChatRequest = {
+        conversationid: this.currentConversation.id,
+        user: this.userObject,
+        message: this.chatInput
+      }
+      console.log(request);
       
-      /*this.http.post(`${this.api_url}`, messageRequest).subscribe((response) => {
-        console.log(response);
-        
-      })*/
+      this.http.post<Chat>(`${this.api_url}/chat`, request).subscribe((response: Chat) => {
+        console.log(response);  
+        this.chat=[...this.chat!,response]
+      })
     }
 }

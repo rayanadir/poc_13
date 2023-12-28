3e83a497bd3bd8591728ec754523e35d653e2b4f
rayanadir <rayan.dahmena@gmail.com>
2023-12-29 00:02:33 +0100

    update conversation request

--- server/src/main/java/com/rayan/server/payload/request/NewConversationRequest.java
+++ server/src/main/java/com/rayan/server/payload/request/NewConversationRequest.java
@@ -2,14 +2,18 @@ package com.rayan.server.payload.request;
 
 import com.rayan.server.models.Customer;
 import com.rayan.server.models.CustomerServiceModel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewConversationRequest {
     @NonNull
-    private Customer customer;
+    private Long customerId;
 
     @NonNull
-    private CustomerServiceModel customerServiceModel;
+    private Long customerServiceModelId;
 }

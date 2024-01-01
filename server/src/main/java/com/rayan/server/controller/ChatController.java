eedf4aad298236e0d89b863519b947d703e5a4eb
rayanadir <rayan.dahmena@gmail.com>
2024-01-01 01:16:40 +0100

    get and send messages

--- server/src/main/java/com/rayan/server/controller/ChatController.java
+++ server/src/main/java/com/rayan/server/controller/ChatController.java
@@ -5,14 +5,20 @@ import com.rayan.server.models.Conversation;
 import com.rayan.server.payload.request.NewChatRequest;
 import com.rayan.server.services.ChatService;
 import com.rayan.server.services.ConversationService;
+import com.rayan.server.services.WebSocketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-
+@Controller
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
@@ -23,6 +29,10 @@ public class ChatController {
     @Autowired
     private ConversationService conversationService;
 
+    @Autowired
+    private WebSocketService webSocketService;
+
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getChatMessages(@PathVariable Long id){
         Conversation conversation = this.conversationService.findConversationById(id);
@@ -31,10 +41,30 @@ public class ChatController {
     }
 
     @PostMapping()
-    public ResponseEntity<?> sendChatMessage(@RequestBody NewChatRequest request){
+    public ResponseEntity<?> sendChatMessageHttp(@RequestBody NewChatRequest request){
         Conversation conversation = this.conversationService.findConversationById(request.getConversationid());
         Chat chat = new Chat(conversation,request.getUser(), request.getMessage(), LocalDateTime.now(), LocalDateTime.now());
         this.chatService.sendChatMessage(chat);
         return ResponseEntity.ok().body(chat);
     }
+
+    @MessageMapping("/get_chat_messages/{conversationId}")
+    @SendTo("/topic/getMessages/{conversationId}")
+    public List<Chat> getChatMessagesSocket(@Payload @DestinationVariable Long conversationId){
+        Conversation conversation = this.conversationService.findConversationById(conversationId);
+        List<Chat> messages = this.chatService.findAllByConversation(conversation);
+        return messages;
+    }
+
+    @MessageMapping("/sendMessage/{conversationId}")
+    @SendTo("/topic/message_sent/{conversationId}")
+    public Chat sendChatMessageSocket(@Payload Chat messageRequest,@DestinationVariable Long conversationId){
+        Conversation conversation = this.conversationService.findConversationById(conversationId);
+        Chat chat = new Chat(conversation,messageRequest.getUser(), messageRequest.getMessage(), LocalDateTime.now(), LocalDateTime.now());
+        this.chatService.sendChatMessage(chat);
+        return chat;
+    }
+
+
+
 }
